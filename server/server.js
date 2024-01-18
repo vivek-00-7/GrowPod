0322d0dc6ec9df6621ad8bf53b10225ab84b5571
vivek-00-7 <vivekarra97@gmail.com>
2024-01-18 18:27:42 +0530

    Add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//express initialization code
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,4 +9,18 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+//mongo initialization code
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://vivekarra97:mongovivek@cluster166.epsesb4.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
